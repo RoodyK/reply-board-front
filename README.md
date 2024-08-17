9090fea2d52d5b25259cd84ef1cea38e2ff9f14c
RoodyK <pps8853@gmail.com>
2024-08-17 18:21:13 +0900

    vuejs 프로젝트 초기화
    `npm i vue@latest` 를 사용한 프로젝트 생성
    불필요한 파일들 제거
    파비콘 설정

---
+++ README.md
@@ -0,0 +1,4 @@
+# reply-board-front
+
+reply-board 프로젝트의 front 부분을 vue로 생성
+
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <h2>Reply Board</h2>
+
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
